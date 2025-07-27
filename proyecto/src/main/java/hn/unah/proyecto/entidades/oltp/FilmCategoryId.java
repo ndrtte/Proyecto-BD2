e28e35ee6b2864671b29913f6556dd626d44df7e
kppa51 <karlapatriciasr21@gmail.com>
2025-07-26 20:23:51 -0600

    .

--- proyecto/src/main/java/hn/unah/proyecto/entidades/oltp/FilmCategoryId.java
+++ proyecto/src/main/java/hn/unah/proyecto/entidades/oltp/FilmCategoryId.java
@@ -13,6 +13,22 @@ public class FilmCategoryId implements Serializable {
 
     private Integer filmId;
     private Integer categoryId;
+
+    public Integer getFilmId() {
+        return filmId;
+    }
+
+    public void setFilmId(Integer filmId) {
+        this.filmId = filmId;
+    }
+
+    public Integer getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Integer categoryId) {
+        this.categoryId = categoryId;
+    }
 }
 
 
