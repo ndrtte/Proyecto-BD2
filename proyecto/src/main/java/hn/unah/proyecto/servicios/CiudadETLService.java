c9b02cd2da91e87730cc4ef711afd1cd07f0c672
ndrtte <andreamorelaleman@gmail.com>
2025-07-28 20:29:34 -0600

    para ciudad

--- proyecto/src/main/java/hn/unah/proyecto/servicios/CiudadETLService.java
+++ proyecto/src/main/java/hn/unah/proyecto/servicios/CiudadETLService.java
@@ -21,52 +21,51 @@ public class CiudadETLService {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-     private List<Map<String, Object>> extraerCiudadesOLTP(String sqlQuery) {
+    private List<Map<String, Object>> extraerCiudadesOLTP(String sqlQuery) {
         List<Map<String, Object>> registrosOLTP = jdbcTemplate.queryForList(sqlQuery);
         List<Map<String, Object>> registrosOLAP = jdbcTemplate.queryForList("SELECT ID_CIUDAD FROM TBL_CIUDAD");
         List<Map<String, Object>> registros = new ArrayList<>();
 
-        for (Map<String,Object> filaOLTP : registrosOLTP) {
+        for (Map<String, Object> filaOLTP : registrosOLTP) {
             Integer idOLTP = ((Number) filaOLTP.get("CITY_ID")).intValue();
             boolean existeEnOlap = false;
 
-            for (Map<String,Object> filaOLAP : registrosOLAP) {
+            for (Map<String, Object> filaOLAP : registrosOLAP) {
                 Integer idOLAP = ((Number) filaOLAP.get("ID_CIUDAD")).intValue();
 
-                if (idOLTP.equals(idOLAP)){
+                if (idOLTP.equals(idOLAP)) {
                     existeEnOlap = true;
                     break;
                 }
 
-                if(!existeEnOlap){
-                    registros.add(filaOLTP);
-                }
-
+            }
+            if (!existeEnOlap) {
+                registros.add(filaOLTP);
             }
         }
 
         return registros;
     }
 
-    public List<CiudadDTO> transformarCiudades(List<Map<String, Object>> ciudadesOrigen){
+    public List<CiudadDTO> transformarCiudades(List<Map<String, Object>> ciudadesOrigen) {
         List<CiudadDTO> ciudadesDTO = new ArrayList<>();
 
-        for ( Map<String, Object> fila : ciudadesOrigen) {
+        for (Map<String, Object> fila : ciudadesOrigen) {
             Integer id = ((Number) fila.get("CITY_ID")).intValue();
             String nombre = fila.get("CITY").toString();
-            
+
             CiudadDTO dto = new CiudadDTO(id, nombre);
             ciudadesDTO.add(dto);
-        }   
-        
+        }
+
         return ciudadesDTO;
-    } 
+    }
 
     private void cargarCiudadesOLAP(List<CiudadDTO> ciudadesDTO) {
 
         List<DimCiudad> ciudadesDestino = new ArrayList<>();
 
-        for (CiudadDTO dto : ciudadesDTO){
+        for (CiudadDTO dto : ciudadesDTO) {
             DimCiudad entidad = new DimCiudad();
             entidad.setIdCiudad((dto.getId()));
             entidad.setNombreCiudad(dto.getNombre());
@@ -86,5 +85,5 @@ public class CiudadETLService {
     public List<DimCiudad> getAllCiudades() {
         return dimCiudadRepository.findAll();
     }
-    
+
 }
